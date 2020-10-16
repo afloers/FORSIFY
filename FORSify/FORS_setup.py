0c7e4a5e0176cd85ce0b24036ed273231457062b
Andreas Flörs <33418619+afloers@users.noreply.github.com>
2020-10-16 13:00:20 +0200

    added reduction methods

--- FORSify/FORS_setup.py
+++ FORSify/FORS_setup.py
@@ -44,13 +44,88 @@ class FORS_setup:
         self.FORSIfy_path = pathlib.Path(__file__).parent.absolute()
 
     def reduce(self):
-        pass
+        if self.archival == True:
+            print("Using archival data")
+            self.create_archival_data_symlinks()
+        if args.root is not None:
+            ps = PypeItSetup.from_file_root(
+                args.root, "vlt_fors2", extension=".fits", output_path=self.working_dir
+            )
+        else:
+            raise IOError("Need to set -r!")
+        ps.run(setup_only=True, sort_dir=sort_dir, write_bkg_pairs=False)
+        self.read_pypeit_sorted()
+        self.change_flat_type()
+        self.choose_detector_chip()
+        self.use_bias_frames()
+        print(self.sorted)
+        self.write_virt_pypeit_file()
+        pipeline = pypeit.PypeIt(
+            self.tmp.name,
+            verbosity=args.verbosity,
+            reuse_masters=False,
+            overwrite=args.overwrite,
+        )
+        pipeline.reduce_all()
+        self.flux_spectra()
 
     def flux_spectra(self):
-        pass
+        science_dir = os.path.join(os.path.split(self.working_dir)[0], "Science")
+        spec1dfiles = glob.glob(science_dir + "/spec1d*.fits")
+        sensfiles = []
+        objects = self.sorted.loc[
+            self.sorted["frametype"].isin(["standard", "science"])
+        ]
+        self.objectfiles = objects["filename"].values
+        self.corresponding_files = {}
+        for idx, file in enumerate(spec1dfiles):
+            found = False
+            for jdx, objectfile in enumerate(self.objectfiles):
+                if objectfile[:-5] in file:
+                    self.corresponding_files[file] = objectfile
+                    found = True
+                    break
+            if found == False:
+                spec1dfiles.remove(file)
+            else:
+                grism = objects.loc[objects["filename"] == objectfile][
+                    "dispname"
+                ].values[0]
+                sensfiles.append(self.grab_sensfunc(grism))
+        header = fits.getheader(spec1dfiles[0])
+        spectrograph = load_spectrograph(header["PYP_SPEC"])
+
+        # Parameters
+        spectrograph_def_par = spectrograph.default_pypeit_par()
+        par = pypeitpar.PypeItPar.from_cfg_lines(
+            cfg_lines=spectrograph_def_par.to_config(), merge_with=[]
+        )
+
+        # Instantiate
+        FxCalib = fluxcalibrate.FluxCalibrate.get_instance(
+            spec1dfiles, sensfiles, par=par["fluxcalib"]
+        )
+
+    def grab_sensfunc(self, grism):
+        try:
+            path = os.path.join(self.FORSIfy_path, "data/sensfunc/", grism, "*.fits")
+            return glob.glob(path)[0]
+        except:
+            raise IOError("Grism not included!")
 
     def create_archival_data_symlinks(self):
-        pass
+        all_files = []
+        path = os.path.join(
+            self.FORSIfy_path, "archival_calibs", "GRIS300V", "20190922"
+        )
+        for type in ["arc", "flats", "bias"]:
+            filepath = os.path.join(path, type, "*.fits")
+            files_of_type = glob.glob(filepath)
+            all_files += files_of_type
+
+        for file in all_files:
+            filename = os.path.split(file)[1]
+            symlink_force(file, os.path.join(self.data_dir, filename))
 
     def change_flat_type(self):
         flat_type = ",".join(self.flat)
@@ -96,7 +171,17 @@ class FORS_setup:
             f.write(self.virt_file.getvalue())
 
     def make_pypeit_header(self):
-        pass
-
-    def grab_sensfunc(self, grism):
-        pass
+        self.pypeit_header = "[rdx]\n"
+        self.pypeit_header += "spectrograph = vlt_fors2\n"
+        self.pypeit_header += "[calibrations]\n"
+        self.pypeit_header += "[[slitedges]]\n"
+        self.pypeit_header += "sync_predict = nearest\n"
+        if args.sources > 0:
+            self.pypeit_header += "[reduce]\n"
+            self.pypeit_header += "[[findobj]]\n"
+            self.pypeit_header += "maxnumber =" + str(self.sources) + "\n"
+        self.pypeit_header += "setup read\n"
+        self.pypeit_header += "Setup A:\n"
+        self.pypeit_header += "setup end\n"
+        self.pypeit_header += "data read\n"
+        self.pypeit_header += "path " + self.data_dir + "\n"
