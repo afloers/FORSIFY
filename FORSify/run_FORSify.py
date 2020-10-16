17c33b506b4da923691afaf853504c82811ff87c
Andreas Flörs <33418619+afloers@users.noreply.github.com>
2020-10-16 12:43:17 +0200

    added parser

--- FORSify/run_FORSify.py
+++ FORSify/run_FORSify.py
@@ -56,3 +56,66 @@ class FORS_setup:
 
     def grab_sensfunc(self, grism):
         pass
+
+
+def parse_args():
+    argparser = argparse.ArgumentParser(description="Script for running FORSify")
+    group = argparser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "-r", "--root", type=str, default=None, help="Raw data directory"
+    )
+
+    argparser.add_argument(
+        "-c",
+        "--chip",
+        default=1,
+        type=int,
+        choices=[1, 2],
+        help="FORS detector ccd number; can be [1,2].",
+    )
+    argparser.add_argument(
+        "-b",
+        "--bias",
+        default=True,
+        type=bool,
+        help="Specifies whether bias frames are used or not. The default is True.",
+    )
+    argparser.add_argument(
+        "-f",
+        "--flat",
+        default=["illumflat", "pixelflat", "trace"],
+        help='File type of flat field images. Can be any combination of ["illumflat", "pixelflat", "trace"]',
+    )
+    argparser.add_argument(
+        "-s",
+        "--sources",
+        default=0,
+        type=int,
+        help="Maximum number of sources to model/extract. An arbitrary number of sources can be specified with '0'.",
+    )
+    argparser.add_argument(
+        "-v",
+        "--verbosity",
+        type=int,
+        default=2,
+        help="Verbosity level between 0 [none] and 2 [all]",
+    )
+    argparser.add_argument(
+        "-o",
+        "--overwrite",
+        default=True,
+        action="store_true",
+        help="Overwrite any existing files/directories",
+    )
+    argparser.add_argument(
+        "-a",
+        "--archival",
+        default=False,
+        action="store_true",
+        help="If True, use archival calibration data instead.",
+    )
+    return argparser.parse_args()
+
+
+def main(args):
+    pass
